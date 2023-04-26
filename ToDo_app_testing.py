9b2f5eea3d171900df2a8ac7fcf82d1a8c8be9c1
Rao <103417697+CodingAssassins@users.noreply.github.com>
2023-04-26 19:14:38 +0400

    Updated Code #3

--- ToDo_app_testing.py
+++ ToDo_app_testing.py
@@ -13,13 +13,49 @@ from datetime import datetime
 import numpy as np
 
 
-def modify_panel(panel: Panel, text: str, add_text: bool) -> Panel:
-    if add_text:
-        panel.body += text
-    else:
-        panel.body = panel.body.replace(text, '')
-    return panel
+def modify_panel(panel_text, add_text=None, remove_text=None):
+    """
+    Modifies text in a Rich panel.
 
-my_panel = Panel("Hello, world!")
-my_panel = modify_panel(my_panel, "world", False) # remove "world" from panel
-my_panel = modify_panel(my_panel, "goodbye", True) # add "goodbye" to panel
+    Args:
+        panel_text (str): Existing text in the panel.
+        add_text (str, optional): Text to be added to the panel. Defaults to None.
+        remove_text (str, optional): Text to be removed from the panel. Defaults to None.
+
+    Returns:
+        str: Updated text in the panel.
+    """
+    # Convert input panel_text to a list for easy modification
+    panel_text_list = panel_text.split('\n')
+    
+    if add_text:
+        panel_text_list.append(add_text)
+        
+    if remove_text:
+        panel_text_list.remove(remove_text)
+    
+    # Convert the modified list back to a string and return it
+    return '\n'.join(panel_text_list)
+
+# Define initial panel text
+panel_text = "This is some text in a Rich panel.\nHere is another line of text.\nAnd a third line!"
+
+# Create the initial panel with the text
+panel = Panel(panel_text, title="My Panel")
+
+# Display the panel
+print(panel)
+
+# Modify the panel by adding a line of text
+panel_text = modify_panel(panel_text, add_text="Here is a new line of text!")
+panel.update(panel_text)
+
+# Display the modified panel
+print(panel)
+
+# Modify the panel by removing a line of text
+panel_text = modify_panel(panel_text, remove_text="And a third line!")
+panel.update(panel_text)
+
+# Display the modified panel
+print(panel)
